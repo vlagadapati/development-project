5c2f847a7623feb58d6f2e13399e0a358080e795
vlagadapati <narayana.lagadapati@gmail.com>
2023-04-12 14:43:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ this is for the testing the new branch in git
---
+++ app.py
@@ -0,0 +1,9 @@
+import boto3
+import json
+for i in range (100):
+    if i%2 ==0:
+        print(f'{i} is an EVEN number')
+        else:
+        prinf(f'{i}is an ODD number')
+        
+        
